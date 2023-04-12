383fa3fd21cd762eefea5adc72450512a50aac34
CabosManuel <cabos.apolaya@gmail.com>
2023-04-12 18:20:10 -0500

    Primer archivio index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+// <?php: Etiqueta de apertura
+
+// echo: Imprimir texto
+// \n  : Salto de línea
+echo "?"."> Etiqueta de cierre PHP, solo es obligatoria cuando se trabaja con código PHP y HTML.\n";
+
+// $   : Declarar variables
+$name = "Manuel";
+$lastName = "Cabos";
+
+// .   : Concatenar texto
+//echo "    - " . $name . " " . $lastName . "\n";
+echo "     - $name $lastName\n";
+echo "4 x 5 = ". 4 * 5;
+
+// ? > : Etiqueta de cierre PHP, solo es obligatoria cuando se trabaja con código PHP y HTML.
