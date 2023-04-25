907a339f97baaec8c8ebf750396b0f1f76808695
CabosManuel <cabos.apolaya@gmail.com>
2023-04-25 16:15:56 -0400

    Agregar data-types.php

---
+++ 02-data-types/01-data-types.php
@@ -0,0 +1,22 @@
+<?php
+
+$number = "16";
+$newNumber = $number / 2;
+
+var_dump($number);
+var_dump($newNumber);
+echo "\n";
+
+
+$number = 32;
+$number = $number * 0.5;
+
+var_dump($number);
+echo "\n";
+
+// (in)
+$cookies = "20 cookies wow";
+$howMany =  6 * $cookies;
+
+echo $howMany;
+echo "\n";
