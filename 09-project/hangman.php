e6bc699b2d3dd2dd31ad7d8e48b94cea3d72075a
CabosManuel <mcabos.dev@gmail.com>
2025-05-09 11:17:24 -0500

    ✨ feat(hangman): Finish logic

--- 09-project/hangman.php
+++ 09-project/hangman.php
@@ -1,5 +1,14 @@
 <?php
 
+function clear() {
+    if (PHP_OS === "WINNT") {
+        // system("cls");
+        popen('cls', 'w');
+    } else {
+        system("clear");
+    }
+}
+
 $possible_words = ["board", "paper", "cable", "table", "glasses", "bottle"];
 
 define("MAX_ATTEMPTS", 6);
@@ -13,25 +22,39 @@ $word_length = strlen($chosen_word);
 $discovered_letters = str_pad("", $word_length, "_");
 $attempts = 0;
 
-echo "$word_length letters word\n\n";
-echo $discovered_letters . "\n\n";
+do {
+    echo "$word_length letters word\n\n";
+    echo $discovered_letters . "\n\n";
 
-// We ask the user to type
-$player_letter = readline("Write a letter: ");
-$player_letter = strtolower($player_letter);
+    // We ask the user to type
+    $player_letter = readline("Write a letter: ");
+    $player_letter = strtolower($player_letter);
 
-if (str_contains($chosen_word, $player_letter)) {
-    // Verify all occurrences of the letter to replace it
-    $offset = 0;
-    while (
-        ($letter_position = strpos($chosen_word, $player_letter, $offset)) !== false
-    ) {
-        $discovered_letters[$letter_position] = $player_letter;
-        $offset = $letter_position + 1;
+    if (str_contains($chosen_word, $player_letter)) {
+        // Verify all occurrences of the letter to replace it
+        $offset = 0;
+        while (
+            ($letter_position = strpos($chosen_word, $player_letter, $offset)) !== false
+        ) {
+            $discovered_letters[$letter_position] = $player_letter;
+            $offset = $letter_position + 1;
+        }
+    } else {
+        clear();
+        $attempts++;
+        echo "❌ Wrong letter. You have " . (MAX_ATTEMPTS - $attempts) . " tries.";
+
+        sleep(2);
     }
-} else {
-    $attempts++;
-    echo "❌ Wrong letter. You have " . (MAX_ATTEMPTS - $attempts) . " tries.";
+    clear();
+} while ( $attempts < MAX_ATTEMPTS && $discovered_letters !== $chosen_word );
 
-    sleep(2);
-}
+clear();
+
+if ( $attempts < MAX_ATTEMPTS )
+    echo "Congrats! You win 🏆\n";
+else
+    echo "You lose 😔\n";
+
+echo "Word: $chosen_word\n";
+echo "You word: $discovered_letters\n";
